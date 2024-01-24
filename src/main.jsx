b5197b9f8c995371ffc1cdac4112c3f4cdf218bf
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2024-01-24 12:25:54 -0300

    init commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyle  from './styles/global'
+
+import theme from './styles/theme.js'
+
+import { Perfil } from './pages/Perfil'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Perfil />
+    </ThemeProvider>
   </React.StrictMode>,
 )
