a202a76d86913e68ba531612b8ba41ac6254066e
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2024-01-31 18:48:32 -0300

    CreateMoveis

--- src/pages/CreateMoveis/index.jsx
+++ src/pages/CreateMoveis/index.jsx
@@ -1,11 +1,21 @@
-import { Container } from './styles'
+import { Container, Form } from './styles'
 import { Header } from '../../components/Header';
 import { TextArea } from '../../components/TextArea';
+import { Input } from '../../components/Input';
+
 export function CreateMoveis(){
   return(
     <Container>
       <Header></Header>
-      <TextArea placeholder='Insira a sua descrição'></TextArea>
+      <Form>
+        <h2>Novo Filme</h2>
+        <div>
+          <Input placeholder='Titulo'></Input>
+          <Input placeholder='Sua nota (de 0 a 5)'></Input>
+        </div>
+        <TextArea placeholder='Observações'></TextArea>
+      </Form>
+      
     </Container>
   );
 }
--- src/pages/CreateMoveis/styles.js
+++ src/pages/CreateMoveis/styles.js
@@ -1,5 +1,24 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  /* max-width: 1137px;
+  margin: 0 auto; */
   
+`;
+
+
+export const Form = styled.div`
+  max-width: 1137px;
+  margin: 0 auto;
+  
+  >h2{
+    margin-bottom: 4rem;
+  }
+
+  > div:nth-child(2){
+    width: 100%;
+    display: flex;
+    gap: 2rem;
+    margin-bottom: 1rem;
+  }
 `;
